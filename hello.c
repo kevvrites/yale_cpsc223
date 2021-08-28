1f1142fe3822373776c35e8323702077cd225d75
kevvrites <liukproductivity@gmail.com>
2021-08-28 14:47:38 -0400

    Completed Hello World Program

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* Hello World Program
+ * Date: 08/28/2021
+ * --------------------------------------------
+ * Write a program in a single file called hello.c that prints Hello, world! as the first line of output.
+ * Note that that first line contains a single space to delimit the words and a newline character at the end.
+ * It is also case-sensitive. You may output anything or nothing after the first line.
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n")
+}
